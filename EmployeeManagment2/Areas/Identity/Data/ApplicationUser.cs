b089693f88042fc1b527d1367fd61581661bcefd
arwaz khan <arwazkhan801@gmail.com>
2022-06-06 18:05:40 +0530

    Add project files.

---
+++ EmployeeManagment2/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace EmployeeManagment2.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+    public string? Name { get; set; }
+
+    public string? EmailId { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public string? DateOfBirth { get; set; }
+}
+
